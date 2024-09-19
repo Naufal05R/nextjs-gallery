b99e57cad78cee454b2b63ac8206d505e48ab3bc
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-19 22:00:31 +0700

    feat: creating list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div className="grid items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
+    <main className="grid grid-cols-3 items-center p-8 pb-20 gap-2 sm:p-20">
+      {Array.from({ length: 200 }).map((_, i) => (
+        <div key={i} className="w-full aspect-[3/2] rounded-xl bg-gray-200"></div>
+      ))}
+    </main>
   );
 }
