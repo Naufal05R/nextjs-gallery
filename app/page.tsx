80503008a5f1c13452b6057cfa3e7b4febc49a92
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-20 09:25:16 +0700

    style: formatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <main className="grid grid-cols-3 items-center p-8 pb-20 gap-2 sm:p-20">
       {Array.from({ length: 200 }).map((_, i) => (
-        <div key={i} className="w-full aspect-[3/2] rounded-xl bg-gray-200"></div>
+        <div
+          key={i}
+          className="w-full aspect-[3/2] rounded-xl bg-gray-200"
+        ></div>
       ))}
     </main>
   );
