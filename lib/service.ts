b92ff2878560823db2239030a03e70f0a0c300c8
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-21 22:31:24 +0700

    feat: update service configuration

--- lib/service.ts
+++ lib/service.ts
@@ -1,9 +1,15 @@
 import * as Minio from "minio";
 
+const END_POINT: string = process.env.MINIO_END_POINT || "";
+const SERVER_PORT: number = parseInt(process.env.MINIO_SERVER_PORT || "");
+const SECURE_URL: boolean = process.env.MINIO_SECURE_URL === "true";
+const ACCESS_KEY: string = process.env.MINIO_ACCESS_KEY || "";
+const SECRET_KEY: string = process.env.MINIO_SECRET_KEY || "";
+
 const minioClient = new Minio.Client({
-  endPoint: "play.min.io",
-  port: 9000,
-  useSSL: true,
-  accessKey: "",
-  secretKey: "",
+  endPoint: END_POINT,
+  port: SERVER_PORT,
+  useSSL: SECURE_URL,
+  accessKey: ACCESS_KEY,
+  secretKey: SECRET_KEY,
 });
