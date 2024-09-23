f771210663449c8cfc88d71861e284f6c99230c7
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-23 15:51:03 +0700

    feat: changing add image button label

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -221,11 +221,13 @@ export default function DashboardPage() {
                     Export
                   </span>
                 </Button>
-                <Button size="sm" className="h-8 gap-1">
-                  <PlusCircle className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Add Product
-                  </span>
+                <Button size="sm" className="h-8 gap-1" asChild>
+                  <Link href="/dashboard/add">
+                    <PlusCircle className="h-3.5 w-3.5" />
+                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                      Add Image
+                    </span>
+                  </Link>
                 </Button>
               </div>
             </div>
