426b1b49cf3b11859220e12df02208556334004f
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-25 20:16:18 +0700

    feat: improving code make it modular and easy to read

--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -8,7 +8,8 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";import { Input } from "@/components/ui/input";
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
@@ -53,15 +54,23 @@ const frameworks = [
   },
 ];
 
-const SelectField = ({ id, type }: { id: string; type: ModelSelectFieldType }) => {
+const SelectField = ({
+  id,
+  title,
+  type,
+}: {
+  id: string;
+  title: string;
+  type: ModelSelectFieldType;
+}) => {
   switch (type) {
     case "void":
-      return <Input id={id} type="text" placeholder="My New Category" />;
+      return <Input id={id} type="text" placeholder={`My New ${title}`} />;
     default:
       return (
         <Select disabled={!type}>
           <SelectTrigger className="w-full">
-            <SelectValue placeholder="Select Category" />
+            <SelectValue placeholder={`Select ${title}`} />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
@@ -77,17 +86,59 @@ const SelectField = ({ id, type }: { id: string; type: ModelSelectFieldType }) =
   }
 };
 
-export default function DashboardAddPage() {
+const SelectFieldType = ({
+  title,
+  fields,
+}: {
+  title: string;
+  fields: string[];
+}) => {
   const [type, setType] = useState<ModelSelectFieldType>("");
 
+  return (
+    <div className="flex flex-col space-y-1.5">
+      <Label>{title}</Label>
+      <RadioGroup
+        defaultValue=""
+        className="flex items-center gap-x-4 py-2"
+        onValueChange={(value: ModelSelectFieldType) => setType(value)}
+      >
+        {fields.map((field) => {
+          return (
+            <Label
+              key={title + field}
+              htmlFor={title + field}
+              className="group flex h-10 flex-1 cursor-pointer items-center rounded-md border bg-transparent px-3 py-2.5 has-[:checked]:border-slate-400 has-[:checked]:text-slate-800"
+            >
+              <RadioGroupItem
+                value={field.toLowerCase()}
+                id={title + field}
+                hidden
+              />
+              <span className="capitalize">Select {field}</span>
+              <Check
+                size={16}
+                className="invisible ml-auto group-has-[:checked]:visible"
+              />
+            </Label>
+          );
+        })}
+      </RadioGroup>
+
+      <SelectField id={title.toLowerCase()} title={title} type={type} />
+    </div>
+  );
+};
+
+export default function DashboardAddPage() {
   const modelCategoryObject = getEnumObject(ModelSelectFieldEnum);
 
   return (
     <main className="grid h-screen w-full place-items-center px-4 py-2 sm:px-8 sm:py-4 lg:px-16 lg:py-8">
       <Card className="w-full max-w-96">
         <CardHeader>
-          <CardTitle>Create image</CardTitle>
-          <CardDescription>Create your image.</CardDescription>
+          <CardTitle>Create Image</CardTitle>
+          <CardDescription>Create your new Image.</CardDescription>
         </CardHeader>
         <CardContent>
           <form>
@@ -96,53 +147,19 @@ export default function DashboardAddPage() {
                 <Label htmlFor="name">Name</Label>
                 <Input id="name" type="text" placeholder="Name of your image" />
               </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="category">Category</Label>
-                <RadioGroup
-                  defaultValue=""
-                  className="flex items-center gap-x-4 py-2"
-                  onValueChange={(value: ModelSelectFieldType) => setType(value)}
-                >
-                  {modelCategoryObject.map((category) => {
-                    category = `${category}`.toLowerCase();
-                    return (
-                      <Label
-                        key={category}
-                        htmlFor={category}
-                        className="group flex h-10 flex-1 cursor-pointer items-center rounded-md border bg-transparent px-3 py-2.5 has-[:checked]:border-slate-400 has-[:checked]:text-slate-800"
-                      >
-                        <RadioGroupItem value={category} id={category} hidden />
-                        <span className="capitalize">Select {category}</span>
-                        <Check
-                          size={16}
-                          className="invisible ml-auto group-has-[:checked]:visible"
-                        />
-                      </Label>
-                    );
-                  })}
-                </RadioGroup>
-
-                <SelectField id="category" type={type} />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="gallery">Gallery</Label>
-                <Select>
-                  <SelectTrigger id="gallery">
-                    <SelectValue placeholder="Select Gallery" />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <SelectItem value="next">Art</SelectItem>
-                    <SelectItem value="sveltekit">Picture</SelectItem>
-                    <SelectItem value="astro">AI</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
+              {["Category", "Gallery"].map((title) => (
+                <SelectFieldType
+                  key={title}
+                  title={title}
+                  fields={modelCategoryObject}
+                />
+              ))}
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
+          <Button>Create</Button>
         </CardFooter>
       </Card>
     </main>
