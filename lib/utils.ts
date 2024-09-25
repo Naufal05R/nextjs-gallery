8ace0442dd0a77495f8fc3e2713813f3834911bb
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-25 17:11:05 +0700

    feat: exporting new getEnumObject reusable function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+type Enum<E> = Record<keyof E, number | string> & { [k: number]: string };
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getEnumObject<E extends Enum<E>>(value: E) {
+  return Object.values(value).slice(0, Object.values(value).length / 2);
 }
