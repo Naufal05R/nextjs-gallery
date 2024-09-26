dc19be572a39315ca0f13c9d7fad12f9aa1a46eb
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-26 11:01:49 +0700

    feat: customing label component

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,26 +1,43 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+
+interface LabelProps
+  extends React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>,
+    VariantProps<typeof labelVariants> {
+  flags?: "optional" | "required";
+}
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+);
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
+  LabelProps
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
     className={cn(labelVariants(), className)}
     {...props}
-  />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+  >
+    {props.children}
+    {props.flags && (
+      <span
+        className={cn("ml-px text-muted-foreground", {
+          "text-red-500": props.flags === "required",
+          "text-slate-500": props.flags === "optional",
+        })}
+      >
+        {props.flags === "required" ? "*" : "optional"}
+      </span>
+    )}
+  </LabelPrimitive.Root>
+));
+Label.displayName = LabelPrimitive.Root.displayName;
 
-export { Label }
+export { Label };
