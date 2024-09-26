52e20eb7d9a0d1f12888a4ac7ecdb2f09761afd0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-26 12:12:10 +0700

    feat: simplify page

--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { SelectFieldType } from "@/components/SelectField";
+import { InputSeveral } from "@/components/InputSeveral";
+import { SelectField } from "@/components/SelectField";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -13,58 +14,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ModelSelectFieldEnum } from "@/constants/enum";
-import { cn, getEnumObject } from "@/lib/utils";
-import { ImageIcon, X } from "lucide-react";
-import { useState } from "react";
-
-const InputSeveral = () => {
-  const [several, setSeveral] = useState<Array<string>>([]);
-  const [value, setValue] = useState("");
-
-  return (
-    <div className="flex flex-col space-y-4">
-      <Label htmlFor="tags">Tags</Label>
-      <Input
-        id="tags"
-        value={value}
-        onChange={(e) => {
-          const { value } = e.target;
-          setValue(value);
-
-          if (value.endsWith(",")) {
-            setSeveral([...several, value.slice(0, -1)]);
-            setValue("");
-          }
-        }}
-        type="text"
-        placeholder="Add tags to image"
-      />
-
-      <ul
-        className={cn(
-          "flex flex-wrap items-center gap-1.5 rounded-md border p-1.5",
-          {
-            hidden: !several?.length,
-          },
-        )}
-      >
-        {several?.map((tag) => (
-          <li
-            key={tag}
-            className="relative -ml-px w-fit select-none rounded bg-slate-800 py-1 pl-2.5 pr-7 text-sm text-slate-100"
-          >
-            {tag}
-
-            <X
-              size={16}
-              className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+import { getEnumObject } from "@/lib/utils";
+import { ImageIcon } from "lucide-react";
 
 export default function DashboardAddPage() {
   const modelCategoryObject = getEnumObject(ModelSelectFieldEnum);
@@ -86,7 +37,7 @@ export default function DashboardAddPage() {
                 <Input id="name" type="text" placeholder="Name of your image" />
               </div>
               {["Category", "Gallery"].map((title) => (
-                <SelectFieldType
+                <SelectField
                   key={title}
                   title={title}
                   fields={modelCategoryObject}
