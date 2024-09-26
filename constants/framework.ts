faea87089b4c4d1b6f9cf03925b1a33c2a383087
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-26 15:54:43 +0700

    feat: adding new constant just for a while

---
+++ constants/framework.ts
@@ -0,0 +1,22 @@
+export const frameworks = [
+  {
+    value: "nextjs",
+    label: "Nextjs",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxtjs",
+    label: "Nuxtjs",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
