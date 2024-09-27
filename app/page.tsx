558a0c67f01c3ccf4195ea37d27e153024e08feb
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-27 17:18:33 +0700

    feat: adding header to root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <main className="grid grid-cols-3 items-center p-8 pb-20 gap-2 sm:p-20">
-      {Array.from({ length: 200 }).map((_, i) => (
-        <div
-          key={i}
-          className="w-full aspect-[3/2] rounded-xl bg-gray-200"
-        ></div>
-      ))}
-    </main>
+    <>
+      <Header />
+      <main className="grid grid-cols-3 items-center gap-2 p-8 pb-20 sm:p-20">
+        {Array.from({ length: 200 }).map((_, i) => (
+          <div key={i} className="aspect-[3/2] w-full rounded-xl bg-gray-200"></div>
+        ))}
+      </main>
+    </>
   );
 }
