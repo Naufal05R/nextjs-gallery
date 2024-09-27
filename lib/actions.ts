1787c2559a9a0bb3b9eeb03ca43c30459d4d160d
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-27 16:20:56 +0700

    feat: test uploading image to s3 server

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { imageFormFields } from "@/constants/form";
+import { createObject } from "./service";
+import { generateId, getExtension } from "./utils";
 
 export const createImage = async (formData: FormData) => {
   const rawData = imageFormFields
@@ -11,5 +13,31 @@ export const createImage = async (formData: FormData) => {
     })
     .reduce((prev, curr) => ({ ...prev, ...curr }), {});
 
-  console.log(rawData);
+  try {
+    const file = rawData.image as File;
+    const arrayBuffer = await file.arrayBuffer();
+    const imageBuffer = Buffer.from(arrayBuffer);
+
+    const uniqueId = generateId("image");
+    const fileName = `${uniqueId}.${getExtension(file.name)}`;
+
+    const params: Parameters<typeof createObject>[0] = {
+      bucketName: "nextjs-gallery",
+      objectName: fileName,
+      objectStream: imageBuffer,
+      objectMetaData: {
+        name: rawData.name,
+        category: rawData.category,
+        gallery: rawData.gallery,
+        tags: rawData.tags,
+      },
+    };
+
+    const result = await createObject(params);
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
