f352b282d2a904c1d3795b75262807ef11cfcef0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-27 08:40:10 +0700

    feat: creating new lib/schema file to store form schema

---
+++ lib/schema.ts
@@ -0,0 +1,22 @@
+import * as z from "zod";
+const MAX_FILE_SIZE = 1024 * 1024 * 5;
+const ACCEPTED_IMAGE_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+const ACCEPTED_IMAGE_TYPES = ["jpeg", "jpg", "png", "webp"];
+
+export const imageFormSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }).max(32, { message: "Name is too long" }),
+  category: z.string().min(1, { message: "Category is required" }).max(32, { message: "Category is too long" }),
+  gallery: z.string().min(1, { message: "Gallery is required" }).max(32, { message: "Gallery is too long" }),
+  image: z
+    .any()
+    .refine((files) => {
+      return files?.[0]?.size <= MAX_FILE_SIZE;
+    }, `Max image size is 5MB.`)
+    .refine(
+      (files) => ACCEPTED_IMAGE_MIME_TYPES.includes(files?.[0]?.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported.",
+    ),
+  tags: z.string().optional().array().max(5),
+});
+
+export type ImageFormData = z.infer<typeof imageFormSchema>;
