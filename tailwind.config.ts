0956dcc0a2fce65d677a12cc49aa46e140c3ebcf
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-27 15:34:59 +0700

    feat: changing tailwindcss config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ import twAnimate from "tailwindcss-animate";
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
