79a56933bf04e0e76073a26ea51ac08edca928ae
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-28 23:24:26 +0700

    feat: throwing new error when webhooks secret is undifined

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -6,4 +6,10 @@ const clerkClient = createClerkClient({
   secretKey: CLERK_SECRET_KEY,
 });
 
-export async function POST(req: Request) {}
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error("Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
+  }
+}
