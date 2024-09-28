6f0281840859fb4f778a2913a698a695aa37eb77
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-28 23:22:18 +0700

    feat: defining clerkClient

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,1 +1,7 @@
+import { createClerkClient } from "@clerk/nextjs/server";
+
 const CLERK_SECRET_KEY = process.env.CLERK_SECRET_KEY;
+
+const clerkClient = createClerkClient({
+  secretKey: CLERK_SECRET_KEY,
+});
