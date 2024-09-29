02adce80f86185b52effc9993d6a1eab09e9c410
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-29 21:54:36 +0700

    feat: implement createUser function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,12 @@
 "use server";
 
 import { User } from "@prisma/client";
+import { prisma } from "../prisma";
 
-export const createUser = async () => {};
+export const createUser = async (user: User) => {
+  try {
+    const newUser = await prisma.user.create({ data: user });
+
+    return newUser;
+  } catch (error) {}
+};
