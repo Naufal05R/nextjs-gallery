b9d7eebfc0954fa4d53637699cf12c1ccdccda68
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-29 21:54:59 +0700

    feat: adding catching error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,11 +2,14 @@
 
 import { User } from "@prisma/client";
 import { prisma } from "../prisma";
+import { handleError } from "../utils";
 
 export const createUser = async (user: User) => {
   try {
     const newUser = await prisma.user.create({ data: user });
 
     return newUser;
-  } catch (error) {}
+  } catch (error) {
+    handleError(error);
+  }
 };
