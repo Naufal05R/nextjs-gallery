f9e21c9dc32625ba4bfb35f7125e9462d510b78f
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-29 21:31:40 +0700

    feat: adding some name changes

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -6,10 +6,10 @@ const clerkClient = createClerkClient({
   secretKey: CLERK_SECRET_KEY,
 });
 
-export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+export async function POST(request: Request) {
+  const WEBHOOK_SECRET_CLERK = process.env.WEBHOOK_SECRET_CLERK;
 
-  if (!WEBHOOK_SECRET) {
-    throw new Error("Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
+  if (!WEBHOOK_SECRET_CLERK) {
+    throw new Error("Please add WEBHOOK_SECRET_CLERK from Clerk Dashboard to .env or .env.local");
   }
 }
