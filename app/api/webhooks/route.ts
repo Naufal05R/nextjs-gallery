0062348b6f5887c9449db0eec7ea695f16c02f32
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-29 21:34:19 +0700

    feat: returning error response when svix header is empty

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -18,4 +18,8 @@ export async function POST(request: Request) {
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", { status: 400 });
+  }
 }
