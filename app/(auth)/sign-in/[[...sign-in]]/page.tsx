28ea20d34378f4a5ae69355ae19f12633a7930cd
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-30 17:02:09 +0700

    feat: create a proper sign in form to page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,16 @@
-import { SignIn } from "@clerk/nextjs";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Form from "@/components/Form";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <Card className="w-full max-w-96">
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+        <CardDescription>Create a new Account to access all feature.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form.SignIn />
+      </CardContent>
+    </Card>
+  );
 }
