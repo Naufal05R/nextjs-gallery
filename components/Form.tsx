fc05cd38c1671d1341e43daa3e3b7df62ef8a350
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-30 16:55:21 +0700

    feat: implement changes

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,6 +1,6 @@
 import { forwardRef } from "react";
 import { Button } from "./ui/button";
-import { authFormFields, imageFormFields } from "@/constants/form";
+import { signInFormFields, imageFormFields } from "@/constants/form";
 import Input from "@/components/Input";
 // import { createImage } from "@/lib/actions/image.actions";
 
@@ -23,7 +23,7 @@ ImageVariant.displayName = "ImageVariant";
 const SignInVariant = forwardRef(() => {
   return (
     <form /* action={createImage} */>
-      {authFormFields.map(({ ...props }, i) => (
+      {signInFormFields.map(({ ...props }, i) => (
         <Input key={i} {...props} />
       ))}
       <div className="mt-8 flex items-center gap-x-4">
