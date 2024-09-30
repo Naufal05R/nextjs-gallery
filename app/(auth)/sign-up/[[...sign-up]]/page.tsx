3149f386b27416095f0e440fc9285d10f9537212
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-30 17:20:05 +0700

    feat: create a proper ui for sign up page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,16 @@
-import { SignUp } from "@clerk/nextjs";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Form from "@/components/Form";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <Card className="w-full max-w-96">
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>Create a new Account to get into Our Application and access all features.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form.SignUp />
+      </CardContent>
+    </Card>
+  );
 }
