b5fd9b08f11efd7b2d896f4bcd03145224939c81
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-09-30 18:08:40 +0700

    feat: updating tailwindcss config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./constants/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
