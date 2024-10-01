7bb591bc8870d8a865c542480710dd0c9bbb2de4
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-01 20:20:13 +0700

    fix: removing declare global

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,11 +1,7 @@
-declare global {
-  type WithRequired<T, K extends keyof T> = T & Required<Pick<T, K>>;
+type WithRequired<T, K extends keyof T> = T & Required<Pick<T, K>>;
 
-  type Prettify<T> = {
-    [K in keyof T]: T[K];
-  } & {};
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
 
-  type CamelCase<S extends string> = S extends `${infer T} ${infer U}`
-    ? `${Lowercase<T>}${Capitalize<CamelCase<U>>}`
-    : S;
-}
+type CamelCase<S extends string> = S extends `${infer T} ${infer U}` ? `${Lowercase<T>}${Capitalize<CamelCase<U>>}` : S;
