9fe82a2a9d7b8cc5f591c02cefd7cee50caf63e6
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-01 20:14:55 +0700

    feat: declaring new global type

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,11 @@
-declare type WithRequired<T, K extends keyof T> = T & Required<Pick<T, K>>;
+declare global {
+  type WithRequired<T, K extends keyof T> = T & Required<Pick<T, K>>;
 
-declare type Prettify<T> = {
-  [K in keyof T]: T[K];
-} & {};
+  type Prettify<T> = {
+    [K in keyof T]: T[K];
+  } & {};
+
+  type CamelCase<S extends string> = S extends `${infer T} ${infer U}`
+    ? `${Lowercase<T>}${Capitalize<CamelCase<U>>}`
+    : S;
+}
