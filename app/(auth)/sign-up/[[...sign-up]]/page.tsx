a2aa5f3dc56b6a9082bad7e104eeef8e1e354ad5
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-02 10:11:25 +0700

    feat: cleaning page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,16 +1,10 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { FormSignUpVariant } from "@/components/Form";
 
 export default function Page() {
   return (
     <Card className="w-full max-w-96">
-      <CardHeader>
-        <CardTitle>Sign Up</CardTitle>
-        <CardDescription>Create a new Account to get into Our Application and access all features.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <FormSignUpVariant />
-      </CardContent>
+      <FormSignUpVariant />
     </Card>
   );
 }
