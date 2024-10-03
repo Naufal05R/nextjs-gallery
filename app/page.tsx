fadf808b55c7d86774d071f00ab618d1fe814647
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-03 18:29:24 +0700

    feat: implement new style to main container

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import { container } from "@/lib/styles";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="grid grid-cols-3 items-center gap-2 p-8 pb-20 sm:p-20">
+      <main className={cn(container.paddingX, "grid grid-cols-3 items-center gap-2 p-8 sm:p-20 sm:pt-40")}>
         {Array.from({ length: 200 }).map((_, i) => (
           <div key={i} className="aspect-[3/2] w-full rounded-xl bg-gray-200"></div>
         ))}
