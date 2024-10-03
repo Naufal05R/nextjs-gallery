39e9edd9cb4cc77dbed2ba5cfafca9262585e59a
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-03 18:29:09 +0700

    feat: remake header, create a good one

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,45 @@
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { container } from "@/lib/styles";
+import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <div>
+    <header
+      className={cn(
+        container.paddingX,
+        "fixed top-0 z-10 flex h-20 w-full items-center justify-between border-b bg-background",
+      )}
+    >
+      <h1 className="text-2xl font-semibold text-slate-800">nanomage</h1>
       <SignedIn>
-        <UserButton />
-
-        <Link href={"/dashboard"}>Dashboard</Link>
+        <Button
+          asChild
+          variant="secondary"
+          size="sm"
+          className="border border-blue-100 bg-blue-500 text-slate-100 hover:bg-blue-600"
+        >
+          <Link href={"/dashboard"}>Dashboard</Link>
+        </Button>
       </SignedIn>
-    </div>
+      <SignedOut>
+        <fieldset className="flex items-center gap-x-2">
+          <Button asChild variant="secondary" size="sm" className="border text-slate-800">
+            <Link href={"/sign-in"}>Sign In</Link>
+          </Button>
+          <Button
+            asChild
+            variant="secondary"
+            size="sm"
+            className="border border-blue-200 bg-blue-500 text-slate-100 hover:bg-blue-600"
+          >
+            <Link href={"/dashboard"}>Create Image</Link>
+          </Button>
+        </fieldset>
+      </SignedOut>
+    </header>
   );
 };
 
