33cd407b702fb8dc9042289766fd227647ad2a74
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-03 15:46:31 +0700

    feat: simplify sign in page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,10 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { FormSignInVariant } from "@/components/Form";
 
 export default function Page() {
   return (
     <Card className="w-full max-w-96">
-      <CardHeader>
-        <CardTitle>Sign In</CardTitle>
-        <CardDescription>Welcome back! Please enter your credentials to access your account.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <FormSignInVariant />
-      </CardContent>
+      <FormSignInVariant />
     </Card>
   );
 }
