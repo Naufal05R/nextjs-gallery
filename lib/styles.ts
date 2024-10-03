99609380be5553924f9acf8614a3bc49d8aec7a1
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-03 17:37:19 +0700

    feat: create a new container style

--- lib/styles.ts
+++ lib/styles.ts
@@ -2,6 +2,9 @@ const styles = {
   wrapper: {
     input: "w-full flex flex-col space-y-4 mt-4",
   },
+  container: {
+    paddingX: "px-4 sm:px-8 lg:px-16",
+  },
 };
 
 export const { wrapper } = styles;
