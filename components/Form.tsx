c9671012b89a35e7e578392cd78b4ee077607449
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-03 15:14:12 +0700

    feat: implement succesful sign up page

--- components/Form.tsx
+++ components/Form.tsx
@@ -102,61 +102,67 @@ export const FormSignUpVariant = forwardRef(() => {
     }
   };
 
-  // if (verifying) {
+  if (verifying) {
+    return (
+      <>
+        <CardHeader>
+          <CardTitle>Email Verification</CardTitle>
+          <CardDescription>Please check at {values.email || "your@email.com"} mailbox.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleVerify} className="flex flex-col">
+            <Input
+              model="Code"
+              title="Code"
+              value={code}
+              length={6}
+              onValueChange={(value) => setCode(value)}
+              valueType="string"
+            />
+            <Button type="reset" variant="link" className="w-full text-blue-500 underline-offset-2">
+              Didn&apos;t receive a code? Resend
+            </Button>
+            <Button type="submit">Verify</Button>
+          </form>
+        </CardContent>
+      </>
+    );
+  }
+
   return (
     <>
       <CardHeader>
-        <CardTitle>Email Verification</CardTitle>
-        <CardDescription>Please check at {values.email || "your@email.com"} mailbox.</CardDescription>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>Join Us! Create a new Account to get started and access all features.</CardDescription>
       </CardHeader>
       <CardContent>
-        <form onSubmit={handleVerify} className="flex flex-col">
-          <Input
-            model="Code"
-            title="Code"
-            value={code}
-            length={6}
-            onValueChange={(value) => setCode(value)}
-            valueType="string"
-          />
-          <Button type="button" variant="link" className="w-full text-blue-500 underline-offset-2">
-            Didn&apos;t receive a code? Resend
+        <form className="flex flex-wrap justify-between">
+          {signUpFormFields.map(({ ...props }, i) => (
+            <Input
+              key={i}
+              {...props}
+              value={values[replaceToCamelCase(props.title)]}
+              onChange={(ref) => {
+                if (typeof ref === "string") {
+                  setValues({ ...values, [replaceToCamelCase(props.title)]: ref });
+                } else {
+                  setValues({ ...values, [replaceToCamelCase(props.title)]: ref.target.value });
+                }
+              }}
+            />
+          ))}
+          <Button type="button" onClick={handleSubmit} className="mt-8 w-full">
+            Sign Up
           </Button>
-          <Button type="button" onClick={() => console.log(code)} className="mt-8 w-full">
-            Verify
-          </Button>
+          <p className="mt-4 text-sm">
+            Already have an account?{" "}
+            <Link href="/sign-in" className="text-blue-500 underline-offset-2 hover:underline">
+              Sign In
+            </Link>
+          </p>
         </form>
       </CardContent>
     </>
-  );
-  // }
-
-  return (
-    <form className="flex flex-wrap justify-between">
-      {signUpFormFields.map(({ ...props }, i) => (
-        <Input
-          key={i}
-          {...props}
-          value={values[replaceToCamelCase(props.title)]}
-          onChange={(ref) => {
-            if (typeof ref === "string") {
-              setValues({ ...values, [replaceToCamelCase(props.title)]: ref });
-            } else {
-              setValues({ ...values, [replaceToCamelCase(props.title)]: ref.target.value });
-            }
-          }}
-        />
-      ))}
-      <Button type="button" onClick={handleSubmit} className="mt-8 w-full">
-        Sign Up
-      </Button>
-      <p className="mt-4 text-sm">
-        Already have an account?{" "}
-        <Link href="/sign-in" className="text-blue-500 underline-offset-2 hover:underline">
-          Sign In
-        </Link>
-      </p>
-    </form>
   );
 });
 
