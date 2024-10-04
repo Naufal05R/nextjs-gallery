01361cf57dabcbe4a9803cf917e3553d6962be17
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-04 11:26:28 +0700

    feat: enable back lib/actions/service.ts

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -1,43 +1,43 @@
-// "use server";
+"use server";
 
-// import { imageFormFields } from "@/constants/form";
-// import { createObject } from "../service";
-// import { generateId, getExtension } from "../utils";
+import { imageFormFields } from "@/constants/form";
+import { createObject } from "../service";
+import { generateId, getExtension } from "../utils";
 
-// export const createImage = async (formData: FormData) => {
-//   const rawData = imageFormFields
-//     .map(({ title }) => {
-//       return {
-//         [title]: formData.get(title),
-//       };
-//     })
-//     .reduce((prev, curr) => ({ ...prev, ...curr }), {});
+export const createImage = async (formData: FormData) => {
+  const rawData = imageFormFields
+    .map(({ title }) => {
+      return {
+        [title]: formData.get(title),
+      };
+    })
+    .reduce((prev, curr) => ({ ...prev, ...curr }), {});
 
-//   try {
-//     const file = rawData.image as File;
-//     const arrayBuffer = await file.arrayBuffer();
-//     const imageBuffer = Buffer.from(arrayBuffer);
+  try {
+    const file = rawData.image as File;
+    const arrayBuffer = await file.arrayBuffer();
+    const imageBuffer = Buffer.from(arrayBuffer);
 
-//     const uniqueId = generateId("image");
-//     const fileName = `${uniqueId}.${getExtension(file.name)}`;
+    const uniqueId = generateId("image");
+    const fileName = `${uniqueId}.${getExtension(file.name)}`;
 
-//     const params: Parameters<typeof createObject>[0] = {
-//       bucketName: "nextjs-gallery",
-//       objectName: fileName,
-//       objectStream: imageBuffer,
-//       objectMetaData: {
-//         name: rawData.name,
-//         category: rawData.category,
-//         gallery: rawData.gallery,
-//         tags: rawData.tags,
-//       },
-//     };
+    const params: Parameters<typeof createObject>[0] = {
+      bucketName: "nextjs-gallery",
+      objectName: fileName,
+      objectStream: imageBuffer,
+      objectMetaData: {
+        name: rawData.name,
+        category: rawData.category,
+        gallery: rawData.gallery,
+        tags: rawData.tags,
+      },
+    };
 
-//     const result = await createObject(params);
+    const result = await createObject(params);
 
-//     return result;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// };
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
