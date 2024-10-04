f84a48b3d9208874387261e39104d487d6265a32
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-04 09:55:48 +0700

    feat: remove unnecessary fake profile button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -128,27 +128,6 @@ export default function DashboardPage() {
               className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
             />
           </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon" className="overflow-hidden rounded-full">
-                <Image
-                  src="/placeholder-user.jpeg"
-                  width={36}
-                  height={36}
-                  alt="Avatar"
-                  className="overflow-hidden rounded-full"
-                />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
           <SignedIn>
             <UserButton />
           </SignedIn>
