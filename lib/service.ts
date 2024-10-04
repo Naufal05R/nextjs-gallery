e430a491d3bd5b8c806ccd09a55a0e17e89bc671
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-04 11:25:53 +0700

    feat: enabling lib/service.ts file again

--- lib/service.ts
+++ lib/service.ts
@@ -1,82 +1,82 @@
-// import * as Minio from "minio";
-// import internal from "stream";
+import * as Minio from "minio";
+import internal from "stream";
 
-// const END_POINT: string = process.env.MINIO_END_POINT || "";
-// const SERVER_PORT: number = parseInt(process.env.MINIO_SERVER_PORT || "");
-// const SECURE_URL: boolean = process.env.MINIO_SECURE_URL === "true";
-// const ACCESS_KEY: string = process.env.MINIO_ACCESS_KEY || "";
-// const SECRET_KEY: string = process.env.MINIO_SECRET_KEY || "";
+const END_POINT: string = process.env.MINIO_END_POINT || "";
+const SERVER_PORT: number = parseInt(process.env.MINIO_SERVER_PORT || "");
+const SECURE_URL: boolean = process.env.MINIO_SECURE_URL === "true";
+const ACCESS_KEY: string = process.env.MINIO_ACCESS_KEY || "";
+const SECRET_KEY: string = process.env.MINIO_SECRET_KEY || "";
 
-// const minioClient = new Minio.Client({
-//   endPoint: END_POINT,
-//   port: SERVER_PORT,
-//   useSSL: SECURE_URL,
-//   accessKey: ACCESS_KEY,
-//   secretKey: SECRET_KEY,
-// });
+const minioClient = new Minio.Client({
+  endPoint: END_POINT,
+  port: SERVER_PORT,
+  useSSL: SECURE_URL,
+  accessKey: ACCESS_KEY,
+  secretKey: SECRET_KEY,
+});
 
-// export const getAllBuckets = async () => {
-//   try {
-//     const buckets = await minioClient.listBuckets();
+export const getAllBuckets = async () => {
+  try {
+    const buckets = await minioClient.listBuckets();
 
-//     return buckets;
-//   } catch (error) {
-//     console.error("Error getting all buckets: ", error);
-//   }
-// };
+    return buckets;
+  } catch (error) {
+    console.error("Error getting all buckets: ", error);
+  }
+};
 
-// export const createObject = async ({
-//   bucketName,
-//   objectName,
-//   objectStream,
-//   objectSize,
-//   objectMetaData,
-// }: {
-//   bucketName: string;
-//   objectName: string;
-//   objectStream: internal.Readable | Buffer | string;
-//   objectSize?: number;
-//   objectMetaData?: Record<string, unknown>;
-// }) => {
-//   try {
-//     const result = await minioClient.putObject(bucketName, objectName, objectStream, objectSize, objectMetaData);
+export const createObject = async ({
+  bucketName,
+  objectName,
+  objectStream,
+  objectSize,
+  objectMetaData,
+}: {
+  bucketName: string;
+  objectName: string;
+  objectStream: internal.Readable | Buffer | string;
+  objectSize?: number;
+  objectMetaData?: Record<string, unknown>;
+}) => {
+  try {
+    const result = await minioClient.putObject(bucketName, objectName, objectStream, objectSize, objectMetaData);
 
-//     return result;
-//   } catch (error) {
-//     console.error("Error creating object: ", error);
-//   }
-// };
+    return result;
+  } catch (error) {
+    console.error("Error creating object: ", error);
+  }
+};
 
-// export const getObject = async ({
-//   bucketName,
-//   objectName,
-//   filePath,
-//   options,
-// }: {
-//   bucketName: string;
-//   objectName: string;
-//   filePath: string;
-//   options?: Record<string, unknown>;
-// }) => {
-//   try {
-//     const result = await minioClient.fGetObject(bucketName, objectName, filePath, options);
+export const getObject = async ({
+  bucketName,
+  objectName,
+  filePath,
+  options,
+}: {
+  bucketName: string;
+  objectName: string;
+  filePath: string;
+  options?: Record<string, unknown>;
+}) => {
+  try {
+    const result = await minioClient.fGetObject(bucketName, objectName, filePath, options);
 
-//     return result;
-//   } catch (error) {
-//     console.error("Error getting object: ", error);
-//   }
-// };
+    return result;
+  } catch (error) {
+    console.error("Error getting object: ", error);
+  }
+};
 
-// export const streamObjects = ({ bucketName, includeMetadata }: { bucketName: string; includeMetadata?: boolean }) => {
-//   const stream: Minio.BucketStream<Minio.BucketItem> = includeMetadata
-//     ? minioClient.extensions.listObjectsV2WithMetadata(bucketName)
-//     : minioClient.listObjectsV2(bucketName);
+export const streamObjects = ({ bucketName, includeMetadata }: { bucketName: string; includeMetadata?: boolean }) => {
+  const stream: Minio.BucketStream<Minio.BucketItem> = includeMetadata
+    ? minioClient.extensions.listObjectsV2WithMetadata(bucketName)
+    : minioClient.listObjectsV2(bucketName);
 
-//   stream.on("data", function (data) {
-//     console.log("V2", data);
-//   });
+  stream.on("data", function (data) {
+    console.log("V2", data);
+  });
 
-//   stream.on("error", function (err) {
-//     console.log(err);
-//   });
-// };
+  stream.on("error", function (err) {
+    console.log(err);
+  });
+};
