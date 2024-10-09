ccdfb6a577b25e4153150fa7b927087723939e98
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-10-09 17:16:46 +0700

    feat: updating middleware, open some api route

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)", "/api/webhooks/clerk(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/list(.*)",
+  "/api/webhooks/clerk(.*)",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
